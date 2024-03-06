69c4130c7bae6568f70b59b767c64c784733c1c3
surajchauhan2310 <129864653+surajchauhan2310@users.noreply.github.com>
2024-03-06 17:34:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// For Single element
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", abc: "xyz" },
+//   "Hello React!"
+// );
+
+// For Nested Element
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement(
+//       "h1",
+//       { id: "header" },
+//       "Hello, I'm the Header of this page"
+//     )
+//   )
+// );
+
+// For Sibling Elements
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "header1s" },
+      "Hello, I'm the Header 1 of this page"
+    ),
+    React.createElement(
+      "h2",
+      { id: "header2" },
+      "Hello, I'm the Header 2 of this page"
+    ),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement(
+      "h1",
+      { id: "header1s" },
+      "Hello, I'm the Header 1 of this page"
+    ),
+    React.createElement(
+      "h2",
+      { id: "header2" },
+      "Hello, I'm the Header 2 of this page"
+    ),
+  ]),
+]);
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
